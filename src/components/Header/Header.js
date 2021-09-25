6c18330e52da1be8d8e2dde65be3d26475664f29
Rony163 <minhajulislamrony43@gmail.com>
2021-09-26 04:40:20 +0600

    Json data created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>Welcome To Bangladesh Drama Industry</h2>
+            <h1>The Budget of the drama is $1000000</h1>
+        </div>
+    );
+};
+
+export default Header;
